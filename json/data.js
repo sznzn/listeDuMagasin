77da2e4934b0b5fcb354f553866ed2e908a68f75
Xin <lu0045@hotmail.com>
2024-10-02 11:06:06 +0200

    我自己练习用的

---
+++ json/data.js
@@ -0,0 +1,74 @@
+let results = 
+    [
+        {
+            "id": 1,
+            "name": "Lyon formation avancée",
+            "tarif": "799€",
+            "note": 4,
+            "image": "https://picsum.photos/180?random=1"
+        },
+        {
+            "id": 2,
+            "name": "Marseille formation intermédiaire",
+            "tarif": "899€",
+            "note": 5,
+            "image": "https://picsum.photos/180?random=2"
+        },
+        {
+            "id": 3,
+            "name": "Paris formation débutante",
+            "tarif": "499€",
+            "note": 3,
+            "image": "https://picsum.photos/180?random=3"
+        },
+        {
+            "id": 4,
+            "name": "Avignon formation courte",
+            "tarif": "699€",
+            "note": 4,
+            "image": "https://picsum.photos/180?random=4"
+        },
+        {
+            "id": 5,
+            "name": "Nice formation professionnelle",
+            "tarif": "1199€",
+            "note": 5,
+            "image": "https://picsum.photos/180?random=5"
+        },
+        {
+            "id": 6,
+            "name": "Bordeaux formation avancée",
+            "tarif": "799€",
+            "note": 4,
+            "image": "https://picsum.photos/180?random=6"
+        },
+        {
+            "id": 7,
+            "name": "Lille formation express",
+            "tarif": "599€",
+            "note": 3,
+            "image": "https://picsum.photos/180?random=7"
+        },
+        {
+            "id": 8,
+            "name": "Nantes formation spécialisée",
+            "tarif": "999€",
+            "note": 5,
+            "image": "https://picsum.photos/180?random=8"
+        },
+        {
+            "id": 9,
+            "name": "Montpellier formation débutante",
+            "tarif": "549€",
+            "note": 3,
+            "image": "https://picsum.photos/180?random=9"
+        },
+        {
+            "id": 10,
+            "name": "Strasbourg formation intermédiaire",
+            "tarif": "749€",
+            "note": 4,
+            "image": "https://picsum.photos/180?random=10"
+        }
+];
+
